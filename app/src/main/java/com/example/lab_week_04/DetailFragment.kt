4a7292227ce9eefbd89c5555ceb9cb8e567ad0ea
Jheno Syechlo <jhenodahlia@gmail.com>
2024-09-20 10:54:09 +0700

    detail fragment ancur

--- app/src/main/java/com/example/lab_week_04/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -1,4 +1,4 @@
-package com.example.lab_week_03
+package com.example.lab_week_04
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,30 +7,44 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
-import com.example.lab_week_04.R
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class DetailFragment : Fragment() {
-    private val coffeeTitle: TextView?
-        get() = view?.findViewById(R.id.coffee_title)
-    private val coffeeDesc: TextView?
-        get() = view?.findViewById(R.id.coffee_desc)
+    private var coffeeTitle: TextView? = null
+    private var coffeeDesc: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            // Use arguments if needed
+        }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-// Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
-        setCoffeeData(coffeeId)
+
+        val coffeeTitle: TextView = view.findViewById(R.id.coffee_title)
+        val coffeeDesc: TextView = view.findViewById(R.id.coffee_desc)
+
+        // Example: Update the views' text
+        coffeeTitle.text = "Affogato"
+        coffeeDesc.text = "An espresso shot with a scoop of vanilla ice cream."
     }
     fun setCoffeeData(id: Int){
         when(id){
